e1593791027385fb1c9494198a463bd362a8c498
Nilesh Patel <nilesh.patel@y7mail.com>
2013-10-12 02:50:53 +1100

    StaticPages 4th test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -25,16 +25,16 @@ describe "Static pages" do
     #end
   end
 
-  #describe "About page" do
-    #it "should have the content 'About us'" do
-    #  visit '/static_pages/about'
-    #  expect(page).to have_content('About us')
-    #end
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
 
     #it "should have the title 'About Us'" do
     #  visit '/static_pages/about'
     #  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     #end
-  #end
+  end
 
 end
