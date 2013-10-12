b22e72ea85ea210a81cce9350834433511e95f00
Nilesh Patel <nilesh.patel@y7mail.com>
2013-10-13 03:31:18 +1100

    Merge - Static Pages Ch 3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,19 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the content 'Home'" do
       visit '/static_pages/home'
       expect(page).to have_content('Home')
     end
 
-    #it "should have the title 'Home'" do
-    #  visit '/static_pages/home'
-    #  expect(page).to have_title("Home")
-    #end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Home")
+    end
   end
 
   describe "Help page" do
@@ -19,10 +22,10 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
 
-    #it "should have the title 'Help'" do
-    #  visit '/static_pages/help'
-    #  expect(page).to have_title('Help')
-    #end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
   end
 
   describe "About page" do
@@ -31,10 +34,26 @@ describe "Static pages" do
       expect(page).to have_content('About Us')
     end
 
-    #it "should have the title 'About Us'" do
-    #  visit '/static_pages/about'
-    #  expect(page).to have_title('About Us')
-    #end
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
   end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+
+
+
+
 
 end
